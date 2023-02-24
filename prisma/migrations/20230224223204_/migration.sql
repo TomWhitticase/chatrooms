081c93aede85f3ffe66ff058dcf4602c997f2706
TomWhitticase <79092168+TomWhitticase@users.noreply.github.com>
2023-02-24 22:32:39 +0000

    prisma migration, removed description from chatroom

---
+++ prisma/migrations/20230224223204_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Chatroom` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Chatroom" DROP COLUMN "description";
